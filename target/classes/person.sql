1abf4e3aa869817f07f337af8440cd372843801d
--add <bulicka.alexandra@gmail.com>
2024-11-13 03:05:45 +0100

    [refactor] - refactor

--- target/classes/person.sql
+++
@@ -1,9 +0,0 @@
-create TABLE person (
-	person_id SERIAL NOT NULL,
-	firstName VARCHAR(255),
-	lastName VARCHAR(255),
-	age INT,
-	date_of_birth TIMESTAMP WITH TIME ZONE NOT NULL,
-	primary key (person_id) 
-)
-
